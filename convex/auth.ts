38370abda909690fa5cfb60bc21e0d20e618baf2
Leanne Sto. Domingo <65695147+LeanneStoDomingo@users.noreply.github.com>
2025-05-30 22:33:32 -0400

    Add function to clear all auth tables

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,8 @@
 import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
 import GitHub from "@auth/core/providers/github";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
-import { query } from "./_generated/server";
+import { query, internalMutation } from "./_generated/server";
+import { asyncMap } from "convex-helpers";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [
@@ -41,5 +42,36 @@ export const getMe = query({
     }
 
     return { name: user.name, avatar: user.avatar, initials };
+  },
+});
+
+export const clearAll = internalMutation({
+  args: {},
+  handler: async (ctx, args) => {
+    await Promise.all([
+      asyncMap(await ctx.db.query("authAccounts").collect(), (account) =>
+        ctx.db.delete(account._id),
+      ),
+      asyncMap(await ctx.db.query("authRateLimits").collect(), (rateLimit) =>
+        ctx.db.delete(rateLimit._id),
+      ),
+      asyncMap(
+        await ctx.db.query("authRefreshTokens").collect(),
+        (refreshToken) => ctx.db.delete(refreshToken._id),
+      ),
+      asyncMap(await ctx.db.query("authSessions").collect(), (session) =>
+        ctx.db.delete(session._id),
+      ),
+      asyncMap(
+        await ctx.db.query("authVerificationCodes").collect(),
+        (verificationCode) => ctx.db.delete(verificationCode._id),
+      ),
+      asyncMap(await ctx.db.query("authVerifiers").collect(), (verifier) =>
+        ctx.db.delete(verifier._id),
+      ),
+      asyncMap(await ctx.db.query("users").collect(), (user) =>
+        ctx.db.delete(user._id),
+      ),
+    ]);
   },
 });
