6a60e63cd751bc98742472f528107bd17ff658ca
Leanne Sto. Domingo <65695147+LeanneStoDomingo@users.noreply.github.com>
2025-06-01 01:30:48 -0400

    Handle board loading and empty state

--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -4,20 +4,28 @@ import { api } from "../../../convex/_generated/api";
 import { useQuery } from "convex/react";
 
 export default function BattlePage() {
-  const board = useQuery(api.games.getBoard);
-
   return (
     <main>
       <div>Battle</div>
-      <div>
-        {board?.map((turn) => {
-          return (
-            <div key={turn.key}>
-              {turn.turnNumber} - {turn.movie}
-            </div>
-          );
-        })}
-      </div>
+      <GameBoard />
     </main>
+  );
+}
+
+function GameBoard() {
+  const board = useQuery(api.games.getBoard);
+
+  if (!board) return <div>Loading board...</div>;
+
+  if (board.length === 0) return <div>No turns yet</div>;
+
+  return (
+    <div>
+      {board.map((turn) => (
+        <div key={turn.key}>
+          {turn.turnNumber} - {turn.movie}
+        </div>
+      ))}
+    </div>
   );
 }
