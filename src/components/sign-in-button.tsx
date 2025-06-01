91e146fae0ff4e25e5b1aac86fa53b0a90b9609b
Leanne Sto. Domingo <65695147+LeanneStoDomingo@users.noreply.github.com>
2025-06-01 01:06:12 -0400

    Add auth redirects for sign in page

--- src/components/sign-in-button.tsx
+++ src/components/sign-in-button.tsx
@@ -1,25 +1,37 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { Button } from "~/components/ui/button";
 import { SiGithub } from "@icons-pack/react-simple-icons";
+import { getSignInUrl } from "~/lib/utils";
 
 export function SignInButton() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const redirect = decodeURIComponent(searchParams.get("redirect") ?? "");
+
   return (
     <Button asChild>
-      <Link href="/sign-in">Sign In</Link>
+      <Link href={getSignInUrl(redirect || pathname).fullPath}>Sign In</Link>
     </Button>
   );
 }
 
 export function GitHubSignInButton() {
   const { signIn } = useAuthActions();
-  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   return (
-    <Button onClick={() => signIn("github", { redirectTo: pathname })}>
+    <Button
+      onClick={() =>
+        signIn("github", {
+          redirectTo: decodeURIComponent(searchParams.get("redirect") ?? "/"),
+        })
+      }
+    >
       <SiGithub />
       <span>Sign In with GitHub</span>
     </Button>
@@ -28,12 +40,16 @@ export function GitHubSignInButton() {
 
 export function AnonymousSignInButton() {
   const { signIn } = useAuthActions();
-  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   return (
     <Button
       variant="secondary"
-      onClick={() => signIn("anonymous", { redirectTo: pathname })}
+      onClick={() =>
+        signIn("anonymous", {
+          redirectTo: decodeURIComponent(searchParams.get("redirect") ?? "/"),
+        })
+      }
     >
       Continue as Guest
     </Button>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,32 @@
+import { NextResponse } from "next/server";
 import {
   convexAuthNextjsMiddleware,
   createRouteMatcher,
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
+import { getSignInUrl } from "./lib/utils";
 
-// const isSignInPage = createRouteMatcher(["/sign-in"]);
+const isSignInPage = createRouteMatcher(["/sign-in"]);
 const isProtectedRoute = createRouteMatcher(["/battle"]);
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-  //   if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-  //     return nextjsMiddlewareRedirect(request, "/product");
-  //   }
-  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/sign-in");
+  const isAuthenticated = await convexAuth.isAuthenticated();
+
+  if (isSignInPage(request) && isAuthenticated) {
+    // TODO: redirect to dashboard or profile page where
+    // player can click a button/choose when to battle
+    return nextjsMiddlewareRedirect(request, "/battle");
+  }
+
+  if (isProtectedRoute(request) && !isAuthenticated) {
+    const { pathname, redirect } = getSignInUrl(request.nextUrl.pathname);
+
+    const url = request.nextUrl.clone();
+    url.pathname = pathname;
+
+    if (redirect) url.searchParams.set("redirect", redirect);
+
+    return NextResponse.redirect(url);
   }
 });
 
